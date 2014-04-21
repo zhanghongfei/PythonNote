d47f1abcf116df7d22988cd82051479fb3e4b05d
zhanghongfei <zhf.hognfei@gmail.com>
2014-04-21 17:39:32 +0800

    线程池

--- thread_pool_executor.py
+++ thread_pool_executor.py
@@ -4,3 +4,44 @@
     python < 3
         pip install futures
 '''
+
+import time
+
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+
+
+def print_a(a):
+
+    time.sleep(2)
+    return a
+
+
+def test_1():
+    ''' 2 s '''
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+
+        res = [
+            executor.submit(print_a, i)
+            for i in xrange(5)
+        ]
+        print res
+
+        for future in as_completed(res):
+
+            print future.result()
+
+def test_2():
+    '''  10 s'''
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for i in xrange(5):
+            res = executor.submit(print_a, i)
+            print res.result()
+
+
+if __name__ == '__main__':
+
+    test_1()
+    test_2()
